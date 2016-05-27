8865e8d0cc1d3cc96cec7c0d5f443234fa7b9d9e
Olli Salonen <oasalonen@gmail.com>
2016-05-27 10:49:56 +0200

    Ability to run server in repl.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-defaults "0.1.5"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler forces-assemble.handler/app}
--- src/forces_assemble/handler.clj
+++ src/forces_assemble/handler.clj
@@ -1,11 +1,16 @@
 (ns forces-assemble.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.adapter.jetty :as jetty]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "Hello World1")
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn def-server []
+  (defonce server
+    (jetty/run-jetty #'app {:port 8000 :join? false})))
