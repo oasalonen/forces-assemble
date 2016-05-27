011ca9b558eeffee46ca0366cbc110176ae6886e
Olli Salonen <oasalonen@gmail.com>
2016-05-27 12:49:12 +0200

    MongoDB connection.

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [com.novemberain/monger "3.0.2"]
+                 [environ "1.0.3"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler forces-assemble.handler/app}
   :profiles
--- src/forces_assemble/handler.clj
+++ src/forces_assemble/handler.clj
@@ -2,7 +2,19 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]
+            [monger.core :as mg]
+            [monger.collection :as mc]))
+
+;; Vars
+(def mongo-uri
+  (or (env :mongodb-mongolab-uri)
+      "mongodb://localhost/test"))
+(def mongo-connection-result (mg/connect-via-uri mongo-uri))
+(def mongodb (:db mongo-connection-result))
+
+;; Server
 
 (defroutes app-routes
   (GET "/" [] "Hello World1")
