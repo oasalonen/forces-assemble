740d969cf25521647593b3321fb157acd78d9ad1
Olli Salonen <oasalonen@gmail.com>
2016-05-31 20:18:39 +0200

    Trigger script improvement

--- trigger-event.sh
+++ trigger-event.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [ "$1" = "local" ]; then
+if [ "$2" = "local" ]; then
 	DOMAIN=localhost:8443
 else
 	DOMAIN=forces-assemble.herokuapp.com
 fi
 
+AUTH_TOKEN=$1
 SERVER_URL="https://${DOMAIN}/channels/ch1/events"
 EVENT="{\"title\":\"Debug event\", \"body\":\"curl says hello\", \"malicious\":\"boo!\", \"data\": {\"foo\": \"bar\"}}"
+echo "Auth token:" $AUTH_TOKEN
 echo "Event:" $EVENT
 echo "Sending event to:" $SERVER_URL
 set -v
-curl -X POST -H 'Content-Type: application/json' -d "${EVENT}" $SERVER_URL -i -k
+curl -X POST -H 'Content-Type: application/json' -H "Authorization:${AUTH_TOKEN}" -d "${EVENT}" $SERVER_URL -i -k
