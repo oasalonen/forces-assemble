2e2ea82366c68ae3ab0645bb79fece4a95c9ed49
Olli Salonen <oasalonen@gmail.com>
2016-07-10 17:31:24 +0200

    Added missing core async dep.

--- project.clj
+++ project.clj
@@ -16,7 +16,8 @@
                  [com.google.firebase/firebase-server-sdk "3.0.0"]
                  [clojure.java-time "0.2.1"]
                  [danlentz/clj-uuid "0.1.6"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/core.async "0.2.385"]]
   :plugins [[lein-ring "0.9.7"]
             [lein-environ "1.0.3"]]
   :ring {:handler forces-assemble.handler/dev-app}
